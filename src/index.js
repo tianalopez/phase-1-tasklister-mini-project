90489abfe032d6ebbf991d1c9699d1398ca9b494
Tiana Lopez <tianadavin@gmail.com>
2023-09-15 19:22:25 -0700

    Add new function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 // Grab relevant items
 const newTaskDescription = document.getElementById('new-task-description');
+// you can also reference this by saying e.target['new-task-description']
 const createTaskForm = document.querySelector('#create-task-form');
 const newTask = document.querySelector('#tasks');
+const priorityOptions = document.querySelector('#priority');
 
 // Function that adds new task
 function addTask() {
@@ -9,10 +11,28 @@ function addTask() {
   let newButton = document.createElement('button');
   newButton.addEventListener('click', removeTask);
   newButton.textContent = "X";
-  newLi.textContent = newTaskDescription.value;
+
+  //determine priority
+  const priority = priorityOptions.value //"low", "medium", "high"
+  if (priority === "low") {
+    newLi.style.backgroundColor = "green";
+  }
+  else if (priority === "medium") {
+    newLi.style.backgroundColor = "yellow";
+  }
+  else {
+    newLi.style.backgroundColor = "red";
+  }
+
+  newLi.textContent = newTaskDescription.value; // use e.target['new-task-description'].value
   newLi.appendChild(newButton);
   newTask.appendChild(newLi);
+
 }
+addTask()
+
+//Delete all tasks
+// newLi.innerHTML = ""
 
 // Delete task
 function removeTask(e) {
@@ -23,6 +43,7 @@ function removeTask(e) {
 function logSubmit(event) {
   event.preventDefault();
   addTask();
+  event.target.reset();
 }
 
 // function that registers the submit
